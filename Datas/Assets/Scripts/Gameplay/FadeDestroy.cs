a26f5a26ff63c69164135c437081f03754b4aa0a
yagoazedias <yagoluis9@hotmail.com>
2015-09-28 22:26:56 -0300

    Version 0.81

---
+++ Datas/Assets/Scripts/Gameplay/FadeDestroy.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FadeDestroy : MonoBehaviour {
+
+	void Start ()
+    {
+		StartCoroutine(AutoDestroy());
+	}
+	
+    public IEnumerator AutoDestroy()
+    {
+        yield return new WaitForSeconds(1);
+        Destroy(this.gameObject);
+    }
+	
+}
